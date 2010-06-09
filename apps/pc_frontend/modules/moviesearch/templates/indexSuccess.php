0fc36565166e7ce1b872161bb740c4731c0f1917
Mutsumi Imamura <imamura@tejimaya.com>
2010-06-09 10:27:10 +0900

    画像の検索方法をgoogle AJAX searchに変更

--- apps/pc_frontend/modules/moviesearch/templates/indexSuccess.php
+++ apps/pc_frontend/modules/moviesearch/templates/indexSuccess.php
@@ -1,57 +1,52 @@
-<html>
-<head>
-<title>youtube search</title>
-</head>
-<body>
-<!--<p>
-<input type="text" id="query" />
-<input type="submit" value="search" onClick="onClick()"/>
-</p>-->
+<!-- ++Begin Video Search Control Wizard Generated Code++ -->
+<!--
+// Created with a Google AJAX Search Wizard
+// http://code.google.com/apis/ajaxsearch/wizards.html
+-->
 
-<div id="movieSerchFormLine" class="parts searchFormLine">
-<input type="text" id="query" />
-<input type="submit" value="search" onClick="onClick()"/>
+<!--
+// The Following div element will end up holding the Video Search Control.
+// You can place this anywhere on your page.
+-->
+<div id="videoControl">
+<span style="color:#676767;font-size:11px;margin:10px;padding:4px;">Loading...</span>
 </div>
 
-<div class="parts">
-<div class="partsHeading"><h3>Movie Search</h3></div>
+<!-- Ajax Search Api and Stylesheet
+// Note: If you are already using the AJAX Search API, then do not include it
+//       or its stylesheet again
+//
+// The Key Embedded in the following script tag is designed to work with
+// the following site:
+// http://picopicobitzz.com/
+-->
+<script src="http://www.google.com/uds/api?file=uds.js&v=1.0&source=uds-vsw&key=ABQIAAAANSkBraw9rhxsjOrkQW15IRTTRfCWr_LwY5LKw009d8Lkvr5JcBS8f-CYRBS_yHH-fdmJLOh7StaBSg"
+type="text/javascript"></script>
+<style type="text/css">
+@import url("http://www.google.com/uds/css/gsearch.css");
+</style>
 
-<div id="result"></div>
-
-<div class="block">
-<div class="moreinfo">
-</div>
-</div>
-</div>
-</div>
+<!-- Video Search Control and Stylesheet -->
+<script type="text/javascript">
+window._uds_vsw_donotrepair = true;
+</script>
+<script src="http://www.google.com/uds/solutions/videosearch/gsvideosearch.js?mode=new"
+type="text/javascript"></script>
+<style type="text/css">
+@import url("http://www.google.com/uds/solutions/videosearch/gsvideosearch.css");
+</style>
 
 <script type="text/javascript">
-function onClick() {
-    document.getElementById("result").innerHTML = "loading...";
-    var query = document.getElementById("query").value;
-    query = encodeURIComponent(query);
-    var jsonpURL =
-        "http://gdata.youtube.com/feeds/videos?vq=" + query + "&max-results=10&alt=json-in-script&callback=listVideos";
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = jsonpURL
-        var head = document.getElementsByTagName("head")[0];
-    head.appendChild(script);
+function LoadVideoSearchControl() {
+    var options = {
+twoRowMode : true
+    };
+    var videoSearch = new GSvideoSearchControl(
+            document.getElementById("videoControl"),
+            [{ query : "blue angels"}, { query : "vw gti"}, { query : "osborne lights"}, { query : "tv ads"}], null, null, options);
 }
-
-function listVideos(data) {
-    var html = '';
-    if(data.feed.openSearch$totalResults.$t > 0) {
-        var entries = data.feed.entry;
-        for(var i=0;i<entries.length;i++) {
-            html += entries[i].content.$t;
-        }
-    }else{
-        html += "<p>not found</p>";
-    }
-    document.getElementById("result").innerHTML = html;
-}
-
+// arrange for this function to be called during body.onload
+// event processing
+GSearch.setOnLoadCallback(LoadVideoSearchControl);
 </script>
-</body>
-</html>
+<!-- --End Video Search Control Wizard Generated Code-- -->
